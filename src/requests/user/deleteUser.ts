02981ba76621f82360be2c24c641bb84f5726581
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-07-18 20:15:34 +0200

    wip crud user  delete

---
+++ src/requests/user/deleteUser.ts
@@ -0,0 +1,25 @@
+import prisma from '$requests';
+
+export async function deleteUser(userId: string) {
+	try {
+		// Désassocier les transactions de l'utilisateur
+		await prisma.transaction.updateMany({
+			where: { userId },
+			data: { userId: null }
+		});
+
+		// Supprimer les commandes associées à l'utilisateur
+		await prisma.order.deleteMany({
+			where: { userId }
+		});
+
+		// Supprimer l'utilisateur
+		await prisma.user.delete({
+			where: { id: userId }
+		});
+
+		return { success: true };
+	} catch (error) {
+		throw new Error('Error deleting user: ' + error.message);
+	}
+}
--- src/routes/dashboard/utilisateurs/+page.server.ts
+++ src/routes/dashboard/utilisateurs/+page.server.ts
@@ -1,12 +1,12 @@
 import { checkOrRegister } from '$requests/user/checkOrRegister';
-
 import { getAllUsers } from '$requests/user/getAllUsers';
 import { deleteUserSchema } from '$zod/userSchema';
 import { zod } from 'sveltekit-superforms/adapters';
 
 import type { PageServerLoad, Actions } from './$types';
 import { redirect } from '@sveltejs/kit';
-import { superValidate } from 'sveltekit-superforms';
+import { message, superValidate } from 'sveltekit-superforms';
+import { deleteUser } from '$requests/user/deleteUser';
 
 const allowedRoles = ['admin'];
 
@@ -34,4 +34,25 @@ export const load: PageServerLoad = async ({ locals }) => {
 		allUsers,
 		session
 	};
+};
+
+// Action pour supprimer un utilisateur
+export const actions: Actions = {
+	deleteUser: async ({ request }) => {
+		const formData = await request.formData();
+		console.log('Received form data:', formData);
+
+		const id = formData.get('id') as string;
+
+		const form = await superValidate(formData, zod(deleteUserSchema));
+
+		try {
+			await deleteUser(id);
+
+			return message(form, 'User deleted successfully');
+		} catch (error) {
+			console.error('Error deleting user:', error);
+			return { error: 'Failed to delete user' };
+		}
+	}
 };

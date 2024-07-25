ce1c081431f84807d259100b7c9e37d387af5d2f
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-07-25 18:18:31 +0200

    add chartJs

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { Line } from 'svelte-chartjs';
+	import {
+		Chart as ChartJS,
+		LineElement,
+		PointElement,
+		LinearScale,
+		Title,
+		Tooltip,
+		Legend,
+		CategoryScale
+	} from 'chart.js';
 
-	export let data: PageData;
+	// Register the necessary components and scales
+	ChartJS.register(LineElement, PointElement, LinearScale, Title, Tooltip, Legend, CategoryScale);
+
+	const data = {
+		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+		datasets: [
+			{
+				label: 'My First dataset',
+				backgroundColor: 'rgba(75,192,192,0.2)',
+				borderColor: 'rgba(75,192,192,1)',
+				data: [65, 59, 80, 81, 56, 55, 40]
+			}
+		]
+	};
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top'
+			},
+			title: {
+				display: true,
+				text: 'Chart.js Line Chart'
+			}
+		}
+	};
 </script>
 
-<div class="w-[100vw] rts">
-	<h1>Dashboard</h1>
-	<div class="m-5"></div>
-</div>
+<Line {data} {options} />

ff1cfdbccbace8226e81302d8409af5c987d465a
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-07-31 18:59:30 +0200

    restore and delete success

--- src/routes/success/+page.ts
+++
@@ -1,8 +0,0 @@
-// src/routes/success/+page.ts
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({ url }) => {
-	const session_id = url.searchParams.get('session_id');
-
-	return { session_id };
-};

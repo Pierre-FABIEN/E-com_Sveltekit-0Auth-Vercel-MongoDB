df3586f182bb08962d52a3491fe4651729bfb45d
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-07-31 19:12:06 +0200

    fix sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,11 +1,24 @@
+import { getAllPosts } from '$requests/posts/getAllPosts';
 import type { RequestHandler } from '@sveltejs/kit';
 import * as sitemap from 'super-sitemap';
 
 export const prerender = true; // Facultatif, utile si vous utilisez le pré-rendu
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async (event) => {
+	const shopId = await getAllPosts();
+	const shopKey = [];
+
+	shopId.forEach((element) => {
+		shopKey.push(element.id);
+	});
+
+	console.log(shopKey, 'oiuhliuh');
+
 	return await sitemap.response({
 		origin: 'http://localhost:1000', // Remplacez par l'URL de votre site
-		excludePatterns: ['^/dashboard', '^/profile', '^/cancel', '^/success', '^/signin', '^/signout']
+		excludePatterns: ['^/dashboard', '^/profile', '^/signin', '^/signout'],
+		paramValues: {
+			'/shop/[id]': shopKey
+		}
 	});
 };
